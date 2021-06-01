6f857ae80f642ee43a689d7a26f9908a0deee597
robinslevel <56522731+robinslevel@users.noreply.github.com>
2021-06-01 04:02:47 +0200

    Add files via upload

---
+++ js/bier.js
@@ -0,0 +1,38 @@
+console.log("hallo wereld");
+
+//var
+
+var pelement = document.querySelector("namen");
+
+var namen = ["robin", "david", "reasz", "lisa", "john", "Tim"];
+var naam = document.querySelector("p");
+var pelement = document.querySelector("p.naam");
+var randmgetal = Math.random() * 5;
+var sukkel = document.querySelector("sukkel");
+var mybutton = document.querySelector('button');
+
+//code
+console.log(namen[0]);
+
+
+naam.textContent = "tekst";
+console.log(naam);
+
+
+
+pelement.textContent = "tekst";
+
+
+randmgetal = Math.floor(randmgetal);
+console.log(randmgetal);
+
+naam.textContent = "sukkel " + namen[randmgetal];
+
+
+console.log("sukkel");
+
+var mybutton = document.querySelector('button');
+mybutton.addEventListener('click', Function(){
+console.log(robin hij doet het);
+                          };
+
